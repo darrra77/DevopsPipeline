e9e0ce5bf64ca844e74b6a44dac3a49c51c3ecf2
Daria <darashaparenko@gmail.com>
2026-03-30 10:44:35 +0300

    feat: add version function

--- src/bot.py
+++ src/bot.py
@@ -32,3 +32,28 @@ def get_version():
     """Return current version of the bot."""
     return "1.0.1"
 
+
+
+
+def format_message(project, branch, date, pr_url, repo_url):
+    
+    message = (
+        f"<b>Pipeline Report</b>
+
+"
+        f"<b>Project:</b> {project}
+"
+        f"<b>Branch:</b> {branch}
+"
+        f"<b>Date:</b> {date}
+
+"
+        f"<b>Links</b>
+"
+        f"<b>Pull Request:</b> {pr_url}
+"
+        f"<b>Repository:</b> {repo_url}
+"
+    )
+    return message
+
